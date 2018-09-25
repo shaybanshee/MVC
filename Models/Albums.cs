2ded9109f829bcc76874aa563ae7dbe9bfd5f45b
Shani Banerjee <shanibanerjee@gmail.com>
2018-09-25 14:04:39 -0400

    Updated Table

---
+++ Models/Albums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicApp2.Models
+{
+    public class Albums
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string CoverArt { get; set; }
+    }
+}
