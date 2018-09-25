6f1e478ea878f9ae7c485542bbc771f6cf2ee45e
Shani Banerjee <shanibanerjee@gmail.com>
2018-09-25 13:46:15 -0400

    Initial Commit

---
+++ Controllers/InitialController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicApp2.Controllers
+{
+    public class InitialController : Controller
+    {
+        // GET: Initial
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicApp2.Startup))]
+namespace MusicApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
